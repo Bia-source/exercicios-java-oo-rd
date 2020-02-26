1833e95296517cf417017e4bd1e90fafae66928a
Treinamento <Treinamento@RD723937.raiadrogasil.com>
2020-02-26 12:22:09 -0300

    Exercicios Ivo Beatriz

--- src/exercicio02/Funcionario.java
+++
@@ -1,7 +0,0 @@
-package exercicio02;
-
-public abstract class Funcionario {
-
-    // TODO: implemente o código desta classe
-
-}
--- src/exercicio02/TecnicoManutencao.java
+++
@@ -1,7 +0,0 @@
-package exercicio02;
-
-public class TecnicoManutencao {
-
-    // TODO: implemente o código desta classe
-
-}
